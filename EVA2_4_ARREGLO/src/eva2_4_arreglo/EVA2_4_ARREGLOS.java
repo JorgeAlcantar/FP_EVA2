cdb208d80b03c1c60220c3af13e1fd76bc669f1c
JorgeAlcantar <L25550707@chihuahua2.tecnm.mx>
2025-10-01 19:19:50 -0600

    Add files via upload

---
+++ EVA2_4_ARREGLO/src/eva2_4_arreglo/EVA2_4_ARREGLOS.java
@@ -0,0 +1,19 @@
+
+package eva2_4_arreglo;
+
+
+public class EVA2_4_ARREGLOS {
+
+
+    public static void main(String[] args) {
+        //necesitan capturar los datos del grupo
+        //ARREGLOS ---- ESTRUCTURA DE DATOS
+        //SON IDENTIFICADORES
+        //secuencia (consecutiva) de valores
+        int[] arreglo;//se  tiene que crear el arreglo (decir su tamaño)
+        arreglo = new int[1000000];
+        //INT[] arreglo = new int[10];
+        
+    }
+    
+}
